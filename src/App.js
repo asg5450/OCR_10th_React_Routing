3b476108f8c7a71a9ed94358a645c6104c751021
seunggyun <asg5450@gmail.com>
2024-12-04 10:20:46 +0900

    useNavigate, useParams
    페이지 이동 및 url Param값 (:id 등) 사용하기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Videos from "./pages/Videos";
+import Root from "./pages/Root";
+import Home from "./pages/Home";
+import VideoDetail from "./pages/VideoDetail";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <p>Home</p>,
-    errorElement: <p>404 Not Found</p>,
+    path: "/", //사이트 라우터
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "videos", element: <Videos /> },
+      { path: "videos/:videoId", element: <VideoDetail /> },
+    ],
   },
   {
     path: "/videos",
-    element: <p>Videos</p>,
+    element: <Videos />,
   },
 ]);
 
